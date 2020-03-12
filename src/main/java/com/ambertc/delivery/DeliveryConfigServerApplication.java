217e07ba54c23be36cc75254dd88e14dd999e75a
José Roberto <jose.junior@invillia.com>
2020-03-12 17:48:53 -0300

    Initial commit

---
+++ src/main/java/com/ambertc/delivery/DeliveryConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.ambertc.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryConfigServerApplication.class, args);
+	}
+
+}
